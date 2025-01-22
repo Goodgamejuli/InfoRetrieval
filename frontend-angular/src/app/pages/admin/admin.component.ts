98565f175de20b3cc113df26ce4086cdab878c0c
Nico K. <nkunz4@hs-mittweida.de>
2025-01-22 11:15:39 +0100

    Some styles for the admin page

--- frontend-angular/src/app/pages/admin/admin.component.ts
+++ frontend-angular/src/app/pages/admin/admin.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  artistToFetch: string = '';
+  useSpotifyApi: boolean = true;
+  useMusicBrainzApi: boolean = true;
 
+  fetchSongsOfArtist() {
+    if(this.artistToFetch == '' 
+      || (!this.useSpotifyApi && !this.useMusicBrainzApi)) {
+        alert("Artist muss eingegeben werden und mindestens eine API muss zum suchen genutzt werden!")
+        return;
+    }
+
+    console.log('Artist Name:', this.artistToFetch);
+    console.log('Use Spotify API:', this.useSpotifyApi);
+    console.log('Use Spotify MusicBrainzAPI:', this.useMusicBrainzApi);
+  }
 }
