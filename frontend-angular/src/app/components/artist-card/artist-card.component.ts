a557b118414e513e2e31d6104ce3e690df0745cd
Nico K. <nkunz4@hs-mittweida.de>
2025-01-24 17:56:35 +0100

    wip2

--- frontend-angular/src/app/components/artist-card/artist-card.component.ts
+++ frontend-angular/src/app/components/artist-card/artist-card.component.ts
@@ -16,7 +16,7 @@ export class ArtistCardComponent {
   @Input() public coverUrl!: string;
 
   constructor() {
-    this.coverUrl = "../public/assets/song_placeholder.png"
+    this.coverUrl = "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228";
   }
 
 }
